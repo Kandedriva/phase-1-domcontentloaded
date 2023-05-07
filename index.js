eeb422ecc43f35b3cc7b160029c13d383f2743de
drissakande <drissakande@gmail.com>
2023-05-07 12:29:50 -0400

    Add code in index.js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+const paragarapheId = document.getElementById("text");
+paragarapheId.textContent = "This is really cool!";
+});
